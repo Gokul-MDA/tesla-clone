e749341388311fa329287297bc0e47ebdaaee850
Gokul-MDA <gokul.mda@gmail.com>
2021-06-07 01:08:46 +0530

    add scroll effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,134 +1,147 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import MenuIcon from '@material-ui/icons/Menu'
-import CloseIcon from '@material-ui/icons/Close'
-import { selectCars } from '../features/car/carSlice'
-import { useSelector } from 'react-redux'
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
-    z-index: 1;
-    min-height: 60px;
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 20px;
-    top: 0;
-    left: 0;
-    right: 0;
-    font-size: 14px;
+  z-index: 1;
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  font-size: 14px;
 
-    img {
-        height: 18px;
-    }
-`
+  img {
+    height: 18px;
+  }
+`;
 
 const Menu = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
 
-    a {
-        font-weight: 600;
-        text-transform: uppercase;
-        padding: 0 10px;
-        flex-wrap: nowrap;
-    }
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
 
-    @media screen and (max-width: 768px) {
-        display: none;
-    }
-`
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const RightMenu = styled.div`
-    display: flex;
-    align-items: center;
-    a {
-        font-weight: 600;
-        text-transform: uppercase;
-        margin-right: 10px;
-        font-size: 14px;
-    }
-`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    font-size: 14px;
+  }
+`;
 
 const CustomMenu = styled(MenuIcon)`
-    cursor: pointer;
-`
+  cursor: pointer;
+`;
 
 const BurgerNav = styled.div`
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    background: white;
-    width: 300px;
-    z-index: 16;
-    list-style: none;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    text-align: start;
-    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
-    transition: transform 0.2s ease-in-out;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s ease-in-out;
 
-    li {
-        padding: 15px 0;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
 
-        a {
-            font-weight: 600;
-        }
+    a {
+      font-weight: 600;
     }
-`
+  }
+`;
 
 const CustomClose = styled(CloseIcon)`
-    cursor: pointer;
-`
+  cursor: pointer;
+`;
 
 const CloseWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 const Header = () => {
+  const [burgerStatus, setburgerStatus] = useState(false);
 
-    const [burgerStatus, setburgerStatus] = useState(false)
+  const cars = useSelector(selectCars);
 
-    const cars = useSelector(selectCars)
-    
-    return (
-        <Container>
-            <a href="tesla.com">
-             <img src="/images/logo.svg" alt=""/>
-            </a> 
-            <Menu>
+  return (
+    <Container>
+      <a href="tesla.com">
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="tesla.com">
+              {car}
+            </a>
+          ))}
+      </Menu>
+      <RightMenu>
+        <a href="tesla.com">Shop</a>
+        <a href="tesla.com">Tesla Account</a>
+        <CustomMenu onClick={() => setburgerStatus(true)} />
+      </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setburgerStatus(false)} />
+        </CloseWrapper>
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              <a href="tesla.com">{car}</a>
+            </li>
+          ))}
+        <li>
+          <a href="tesla.com">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="tesla.com">Used Inventory</a>
+        </li>
+        <li>
+          <a href="tesla.com">Trade-in</a>
+        </li>
+        <li>
+          <a href="tesla.com">Cybertruck</a>
+        </li>
+        <li>
+          <a href="tesla.com">Roadaster</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
+};
 
-                {cars && cars.map((car, index)=>(
-                    <a key={index} href="tesla.com">{car}</a>
-                ))}                
-               
-            </Menu>
-            <RightMenu>
-                <a href="tesla.com">Shop</a>
-                <a href="tesla.com">Tesla Account</a>
-                <CustomMenu onClick={() => setburgerStatus(true)}/>
-            </RightMenu>
-            <BurgerNav show={burgerStatus}>
-                <CloseWrapper>
-                <CustomClose onClick={() => setburgerStatus(false)}/>
-                </CloseWrapper>
-                {cars && cars.map((car, index)=>(
-                <li key={index}><a href="tesla.com">{car}</a></li>
-                ))}   
-                <li><a href="tesla.com">Existing Inventory</a></li>
-                <li><a href="tesla.com">Used Inventory</a></li>
-                <li><a href="tesla.com">Trade-in</a></li>
-                <li><a href="tesla.com">Cybertruck</a></li>
-                <li><a href="tesla.com">Roadaster</a></li>
-            </BurgerNav>
-        </Container>
-    )
-}
-
-export default Header
+export default Header;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,95 +1,96 @@
-import React from 'react'
-import styled from 'styled-components'
-import Fade from 'react-reveal/Fade'
+import React from "react";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 const Wrap = styled.div`
-    z-index: 10;
-    width: 100vw;
-    height: 100vh;
-    background-image: ${props => `url("/images/${props.bgImage}")`};
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  z-index: 10;
+  width: 100vw;
+  height: 100vh;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  scroll-snap-align: start;
+
+  @media screen and (max-width: 768px) {
     scroll-snap-align: start;
-`
+  }
+`;
 
 const ItemText = styled.div`
-    padding-top: 15vh;
-    text-align: center;
-`
+  padding-top: 15vh;
+  text-align: center;
+`;
 
-const Buttons = styled.div`
-
-`
+const Buttons = styled.div``;
 
 const ButtonGrp = styled.div`
-    display: flex;
-    margin-bottom: 30px;
+  display: flex;
+  margin-bottom: 30px;
 
-    @media screen and (max-width: 768px) {
-        display: flex;
-        flex-direction: column;
-    }
-`
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 const LeftButton = styled.div`
-    background-color: rgba(23, 26, 32, 0.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    border-radius: 100px;
-    align-items: center;
-    justify-content: center;
-    opacity: 0.85;
-    text-transform: uppercase;
-    font-size: 12px;
-    cursor: pointer;
-    margin: 8px;
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  border-radius: 100px;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 
 const RightButton = styled(LeftButton)`
-    background: white;
-    opacity: 0.65;
-    color: black;
-`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
 
 const DownArrow = styled.img`
-    height: 40px;
-    animation: animateDown infinite 1.5s;
-`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
 
-const Section = ({ title, description, backgroundImg, leftBtnText, rightBtnText }) => {
-    return (
-        <Wrap bgImage={backgroundImg}>
-            
-            <Fade bottom>
-                <ItemText>
-                    <h1>{title}</h1>
-                    <p>{description}</p>
-                </ItemText>
-            </Fade>
-            <Buttons>
-                <Fade bottom>
-                    <ButtonGrp>
-                        <LeftButton>
-                            {leftBtnText}
-                        </LeftButton>
-                        {rightBtnText &&
-                            <RightButton>
-                                {rightBtnText}
-                            </RightButton>
-                        }
-                    </ButtonGrp>
-                    <DownArrow src="/images/down-arrow.svg" />
-                </Fade>
-            </Buttons>
-        </Wrap>
-    )
-}
+const Section = ({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) => {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
+      <Buttons>
+        <Fade bottom>
+          <ButtonGrp>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGrp>
+          <DownArrow src="/images/down-arrow.svg" />
+        </Fade>
+      </Buttons>
+    </Wrap>
+  );
+};
 
-export default Section
+export default Section;
